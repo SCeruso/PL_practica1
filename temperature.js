68ab49a62b577f78e4fd4af5043b3154ef4036c8
alu0100764666 <alu0100764666@ull.edu.es>
2015-02-09 18:08:19 +0000

    Hecha la conversion

---
+++ temperature.js
@@ -0,0 +1,27 @@
+// JavaScript source code
+function calculate() {
+    var result;
+    var original= document.getElementById("original");
+    var temp = original.value;
+    var regexp = /([+-]?\d+(?:\.\d+)?)\s*([cCfF])/;
+  
+    var m = temp.match(regexp);
+  
+    if (m) {
+        var num = m[0];
+        var type = m[1];
+        num = parseFloat(num);
+        if (type == 'c' || type == 'C') {
+            result = (num * 9/5)+32;
+            result = result.toFixed(1) + "F"
+        }
+        else {
+            result = (num - 32)*5/9;
+            result = result.toFixed(1) + "C"
+        }
+        converted.innerHTML = result;
+    }
+    else {
+        converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+    }
+}
