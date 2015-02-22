bacde0f2bda7bcab77a5b65cc185d4ac895b1e68
alu0100764666 <alu0100764666@ull.edu.es>
2015-02-22 11:21:59 +0000

    Configurando karma

--- karma.conf.js
+++ karma.conf.js
@@ -10,11 +10,18 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
+
+    client: {
+          mocha: {
+            ui: 'tdd'
+          }
+    },
 
 
     // list of files / patterns to load in the browser
     files: [
+      'tests/index.html',
       '*.js',
       'tests/*.js'
     ],
@@ -29,8 +36,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'tests/index.html': ['html2js']
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
@@ -57,7 +64,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['Firefox', 'PhantomJS'],
 
 
     // Continuous Integration mode
